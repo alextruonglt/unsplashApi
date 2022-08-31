b115220d45d021d7d5c8fc1f891251b2955874fd
Alex Truong <87406255+alextruonglt@users.noreply.github.com>
2022-08-31 15:22:46 -0700

    functionality built out

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import React, { useState, useEffect } from 'react'
 import { FaSearch } from 'react-icons/fa'
 import Photo from './Photo'
-const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
+
+
+// const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
 
 const mainUrl = `https://api.unsplash.com/photos/`
 const searchUrl = `https://api.unsplash.com/search/photos/`
-
-const accesskey = "tsrcbltlD1W_hPeIuOiriTDkL1ATd59g6z59TTro0Bc"
+const accessKey = "?client_id=tsrcbltlD1W_hPeIuOiriTDkL1ATd59g6z59TTro0Bc"
 
 function App() {
 
   const [loading, setLoading] = useState(false)
   const [photos, setPhotos] = useState([])
+  const [page, setPage] =useState(1)
+  const [query, setQuery] = useState("")
 
   const fetchImages = async () =>{
     setLoading(true)
-    let url = `${mainUrl}${clientID}`
+    let url
+    const urlPage = `&page=${page}`
+    const urlQuery = `&query=${query}`
+ 
+
+    if(query){
+      url = `${searchUrl}${accessKey}${urlPage}${urlQuery}`
+    }else{
+      url = `${mainUrl}${accessKey}${urlPage}`
+    }
+
+
     try {
       const response = await fetch(url)
       const data = await response.json()
-      console.log(data)
+      setPhotos((oldPhotos) =>{
+        if(query && page ===1){
+          return data.results
+        }
+        else if(query){
+          return [...oldPhotos, ...data.results ]
+        } else{
+          return [...oldPhotos, ...data]
+        }
+      })
+      setLoading(false)
+
     } catch (error) {
       setLoading(false)
       console.log(error)
@@ -28,10 +53,52 @@ function App() {
 
   useEffect(() =>{
     fetchImages()
+      // eslint-disable-next-line
+  },[page])
+  // eslint-disable-next-line
+
+  useEffect(() =>{
+    const event = window.addEventListener("scroll", () =>{
+
+      if(( !loading &&
+        window.innerHeight + window.scrollY)>= document.body.scrollHeight -2)
+        {
+          setPage((prevState) => prevState +1)
+      }
+
+    })
+    return () => window.removeEventListener("scroll", event)
+    // eslint-disable-next-line
   },[])
 
+  const handleSubmit=(e) =>{
+    e.preventDefault()
+    setPage(1)
+  }
 
-  return <h2>stock photos starter</h2>
+
+  return (
+    <main>
+      <section className="search">
+        <form className="search-form">
+          <input type="text" placeholder='search' className='form-input' value={query}
+          onChange={(e)=> setQuery(e.target.value)}
+          />
+          <button type="submit" className="submit-btn" onClick={handleSubmit}>
+            <FaSearch/>
+          </button>
+        </form>
+      </section>
+      <section className="photos">
+        <div className="photos-center">
+          {photos.map((image, index) =>{
+            return <Photo key={index} {...image} />
+          })}
+        </div>
+        {loading && <h2 className='loading'>Loading...</h2>}
+      </section>
+    </main>
+  )
 }
 
 export default App
